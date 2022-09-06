24c9fcaed2e8ac7920271925b9daa8908590efbe
nam <namgh627@naver.com>
2022-09-06 15:38:17 +0900

    feat/firstsettings

---
+++ README.md
@@ -0,0 +1,46 @@
+# 웨인힐스벤스 과제
+
+기간: 2022/8/30 ~ 2022/9/6
+
+## 팀원
+
+---
+
+1. 나소현(팀장)
+2. 이혁준
+3. 남광후
+4. 정지은
+
+## 개요
+
+---
+
+프리온보딩 백엔드 코스 첫번째 기업과제
+
+과제 : ![](/img/project.png)
+
+역할 분담 및 일정정리: https://www.notion.so/ee168e18f6fc4a2ca745e49fba6df82d
+
+## Erd
+
+![](/img/erd.png)
+
+## api명세서
+
+![](/img/api.png)
+
+## api
+
+### 유저 create & delete
+
+유저 create api 요청을 받을 때 email, password,name 등 필요한 정보를 받아
+생성
+
+![](/img/usercreate.png)
+
+유저 delete api는 id값을 파라미터로 받아서 id값의 정보들을 softdelete
+
+### 로그인
+
+
+### 공지사항 && 운영게시판 && 자유게시판
---
+++ src/app.module.ts
@@ -0,0 +1,38 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './apis/user/user.module';
+import { AuthModule } from './apis/auth/auth.module';
+import { NoticeBoardModule } from './apis/notice_board/notice_board.module';
+import { OperationBoardModule } from './apis/operation_board/operation_board.module';
+import { GeneralBoardModule } from './apis/general_board/general_board.module';
+
+@Module({
+  imports: [
+    UserModule,
+    AuthModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.HOST,
+      port: 3306,
+      username: process.env.USERNAME,
+      password: process.env.PASSWORD,
+      database: process.env.DATABASE,
+      entities: [__dirname + '/apis/**/*.entity.*'],
+      synchronize: true,
+      logging: true,
+    }),
+
+    NoticeBoardModule,
+
+    OperationBoardModule,
+
+    GeneralBoardModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
